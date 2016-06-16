ea81a09153c236e44056dd6fcc7f4b2fff05db40
andrew.m <andrew.m@moneyveo.local>
2016-06-16 16:21:54 +0300

    MVC-201606161621

---
+++ EssentialTools/Models/ShoppingCart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssentialTools.Models
+{
+    public class ShoppingCart
+    {
+        private LinqValueCalculator cp;
+        public ShoppingCart(LinqValueCalculator calcParam)
+        {
+            cp = calcParam;
+        }
+        public IEnumerable<Product> Products { get; set; }
+
+        public decimal CalculateProductTotal()
+        {
+            return cp.ValueProducts(Products);
+        }
+    }
+}
