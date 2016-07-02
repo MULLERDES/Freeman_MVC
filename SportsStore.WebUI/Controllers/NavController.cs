9a62240c9ee4756ed0468547604256373c569b13
andrew.m <andrew.m@moneyveo.local>
2016-07-02 11:22:11 +0300

    MVC-201607021122

--- SportsStore.WebUI/Controllers/NavController.cs
+++ SportsStore.WebUI/Controllers/NavController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SportsStore.Domain.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,16 @@ namespace SportsStore.WebUI.Controllers
 {
     public class NavController : Controller
     {
+
+        private IProductsRepository repository;
+
+        public NavController(IProductsRepository repo)
+        { repository = repo; }
         // GET: Nav
-        public string Menu()
+        public PartialViewResult Menu()
         {
-            return "Hello form ";
+            IEnumerable<string> catgs = repository.Products.Select(s => s.Category).Distinct().OrderBy(o => o);
+            return PartialView(catgs);
         }
     }
 }
